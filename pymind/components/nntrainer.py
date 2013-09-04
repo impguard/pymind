fixed assertion in gradient test

pymind/components/nntrainer.py
@@ -43,7 +43,7 @@ class NNTrainer(object):
       self.nn.weights = weights
 
       # Helper variables
-      m = X.shape[1]
+      m = X.shape[1] 
       bias = 1 if self.nn.bias else 0
 
       # Part 1: Feed-forward + Get error
@@ -51,7 +51,7 @@ class NNTrainer(object):
       # Errors
       error = self.err_fn.calc(a[-1], y)
       # Unregularized Cost
-      cost = (1. / m) * sum(error)
+      cost = (1. / m) * error.sum()
       # Regularized
       cost += (self.learn_rate / 2. * m) * sum(np.sum(np.power(weight[:, bias:], 2)) for weight in self.nn.weights)
 

test/nntrainer_test.py
@@ -26,9 +26,10 @@ def testGradient():
   cst,grd = costFn(wvec)
   # Run computeNumericalGradient with the network costs and weights
   cgrd = computeNumericalGradient(costFn,wvec)
-  for i in range(len(cgrd)):
-    np.testing.assert_array_almost_equal(cgrd[i], grd[i], decimal = 4,
-      err_msg = "The output grd at index %d should be \n %r \n != %r" % (i, cgrd[i], grd[i]))
+  cgrdvec,grdvec = cgrd.T,grd.T
+  for i in range(len(cgrdvec)):
+    np.testing.assert_array_almost_equal(cgrdvec[i], grdvec[i], decimal = 3,
+      err_msg = "The output grd at index %d should be \n %r \n != %r" % (i, cgrdvec[i], grdvec[i]))
 
 """
 Computes the gradient of the weight vector _weights_, given a cost
@@ -36,8 +37,9 @@ function _costFn_ and an optional epsilon _e_. Smaller epsilon values
 allow more accurate estimations of the gradient.
 """
 def computeNumericalGradient(costFn, weights, e=0.01):
-  grd = np.empty(len(weights))
-  for i in xrange(len(weights)):
+  wsize = len(weights)
+  grd = np.empty(wsize)
+  for i in xrange(wsize):
     w = weights[i]
     w_inc = w + e
     w_dec = w - e
@@ -47,4 +49,4 @@ def computeNumericalGradient(costFn, weights, e=0.01):
     c_dec,_ = costFn(weights)
     grd[i] = (c_inc - c_dec)/(2*e)
     weights[i] = w
-  return grd
+  return np.matrix(grd).reshape((1,wsize))