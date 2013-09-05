617049746d51aff94b354384f538f8eacffbacfd
whsieh <whsieh@berkeley.edu>
2013-09-04 17:06:57 -0700

    made gradient test deterministic

--- test/nntrainer_test.py
+++ test/nntrainer_test.py
@@ -17,8 +17,8 @@ def testGradient():
   # Create NNTrainer
   trainer = NNTrainer(nnet,logitError,1)
   # Create Random Data
-  X = np.matrix(nprandom((10,500)))
-  y = np.matrix(nprandom((6,500)))
+  X = np.exp(-np.arange(-1,1,2/5000.0).reshape((10,500))**2)
+  y = np.exp(-np.arange(-1,1,2/3000.0).reshape((6,500))**2)
   # Create Cost Function
   costFn = trainer.createCostFn(X,y)
   # Run Cost Function and retrieve gradient
@@ -35,7 +35,7 @@ Computes the gradient of the weight vector _weights_, given a cost
 function _costFn_ and an optional epsilon _e_. Smaller epsilon values
 allow more accurate estimations of the gradient.
 """
-def computeNumericalGradient(costFn, weights, e=0.01):
+def computeNumericalGradient(costFn, weights, e=0.001):
   wsize = weights.size
   grd = np.empty(wsize)
   for i in xrange(wsize):
