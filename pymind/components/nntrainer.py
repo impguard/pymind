Updated nntrainer.py and nntrainer_test.py to fix gradient accuracy.
Still buggy, does not compute gradient correctly.

pymind/components/nntrainer.py
@@ -13,7 +13,7 @@ class NNTrainer(object):
   def __init__(self, nn):
     self.nn = nn
 
-  def createCostfn(self, X, y, learn_rate, errorfn):
+  def createCostfn(self, X, y, learn_rate, errorfn, computeGrad=True):
     """ Creates a cost function that takes in a list of weights and returns the error cost.
 
     This method creates a method that, given a column vector of weights, first calculates the cost
@@ -27,12 +27,17 @@ class NNTrainer(object):
     Arguments:
     X -- The featureset with each column representing a feature vector for one training example.
     y -- The output vector with each column representing the output vector for one training example.
+    learn_rate -- The learning rate for regularization
+    errorfn -- Error function used when computing cost
+    computeGrad -- Whether or not to compute the gradient or not in addition to the cost
     Returns:
     A cost function that takes a list of weights.
 
     Note: The number of columns in X and y represent the number of training examples, so the number
     of columns of each must match up. Otherwise, a matrix dimension mismatch error will be raised.
     """
+    learn_rate = float(learn_rate)
+
     def costfn(weights):
       """ The cost function created by createCostfn.
 
@@ -40,42 +45,76 @@ class NNTrainer(object):
       of weights. The weights should be correctly shaped. The cost function depends on the error
       function provided and uses regularization. How much regularization is used can be tuned with
       the learning rate (lambda).
+      Returns:
+      The cost and, if computeGrad is True, the gradient of the neural network.
       """
       try:
         # Set the weights of the neural network
         self.nn.weights = weights
 
         # Helper variables
-        m = float(X.shape(1)) # Number of training examples
+        m = float(X.shape[1]) # Number of training examples
         bias = 1 if self.nn.bias else 0
 
         # Part 1: Feed forward and get cost
         z, a = self.nn.feed_forward(X)
         h = a[-1] # Hypothesis
         err_vector = errorfn.calc(h, y)
-        unreg_cost = (1. / m) * error.sum() # Unregularized cost
+        unreg_cost = (1. / m) * err_vector.sum() # Unregularized cost
         sum_squared_weights = np.sum([np.square(weight[:, bias:]).sum() for weight in weights])
         reg_cost = (learn_rate / (2 * m)) * sum_squared_weights # Regularized cost
         cost = reg_cost + unreg_cost
 
-        return cost
+        if computeGrad:
+          # Part 2: Backpropogation and get grad
+          d = deque()
+
+          # Calculate deltas
+          d.appendleft(np.multiply(self.nn.layers[-1].activationfn.grad(z[-1]), errorfn.grad(h, y)))
+
+          for i in range(len(self.nn.layers) - 2, 0, -1):
+            prevD = d[0]
+            activationfn = self.nn.layers[i].activationfn
+            weight = weights[i]
+            d.appendleft(np.multiply(activationfn.grad(z[i]), weight[:, bias:].T * prevD))
+
+          d.appendleft(None) # Filler since d0 is unimportant
+
+          # Calculate gradients
+          grads = list()
+          for i in range(len(self.nn.weights)):
+            # Unregularized gradient
+            unreg_grad = (1. / m) * d[i+1] * a[i].T
+            # Regularized gradient excluding bias
+            reg_grad = (learn_rate / m) * weights[i][:, bias:]
+            # Create gradient
+            grad = unreg_grad
+            grad[:, bias:] += reg_grad
+            grads.append(grad)
+
+          return cost, grads
+        else:
+          return cost
       except ValueError:
         print "Calculating cost of a neural network failed. Most likely due to dimension mismatch."
         raise
+    return costfn
 
-  def reshapeWeights(unrolled_weights):
+  def reshapeWeights(self, unrolled_weights):
     """ Reshapes an unrolled column vector of weights into the proper sizes.
 
     Assumes that the correct number of weights are passed, and uses the neural network's weight
     shapes and sizes to determine how to reshape the unrolled weights.
+
     Arguments:
     unrolled_weights -- A column vector of unrolled weights
     Returns:
-    A list of matrices for each reshaped weight matrix
+    A list of matrices for each reshaped weight matrix (copy).
 
     Note: This method assumes that the size of unroll_weights is correct.
     """
     try:
+      unrolled_weights = np.matrix(unrolled_weights)
       weights = list()
       curr_index = 0
       for weight in self.nn.weights:
@@ -89,67 +128,16 @@ class NNTrainer(object):
       print "Reshaping weights failed. Most likely due to incorrect size of unrolled_weights."
       raise
 
+  def unrollWeights(self, weights):
+    """ Unrolls a list of weights into a column vector.
 
-  """
-  def train(self, X, y):
-    costFn = self.createCostFn(X, y)
-    # Minimize!
+    Presumes that a list of weights of the proper dimensions are passed. Technically does not need
+    to be bound to nntrainer, but unrolling is a common function being used in trainer, so this
+    is simply a helper function.
 
-    # x, f, d = fmin_l_bfgs_b(costFn, np.vstack(self.nn.weights).flatten())
-
-  def createCostFn(self, X, y):
-    def costFn(params):
-      # Reshape params to get weights
-      weights = list()
-      curr_index = 0
-      for weight in self.nn.weights:
-        shape = weight.shape
-        size = weight.size
-        weights.append(np.matrix(params[curr_index:curr_index + size].reshape(shape)))
-        curr_index += size
-
-      # Set the weights of the neural network
-      self.nn.weights = weights
-
-      # Helper variables
-      m = float(X.shape[1])
-      bias = 1 if self.nn.bias else 0
-
-      # Part 1: Feed-forward + Get error
-      z, a = self.nn.feed_forward(X)
-      # Errors
-      error = self.err_fn.calc(a[-1], y)
-      # Unregularized Cost
-      cost = (1. / m) * error.sum()
-      # Regularized
-      cost += self.learn_rate / (2. * m) * sum(np.sum(np.power(weight[:, bias:], 2)) for weight in self.nn.weights)
-
-      # Part 2: Backpropogation
-      d = deque()
-
-      lastD = np.multiply(self.nn.layers[-1].activationfn.grad(z[-1]), self.err_fn.grad(a[-1], y))
-
-      d.appendleft(lastD)
-
-      for i in range(len(self.nn.layers) - 2, 0, -1):
-        fn = self.nn.layers[i].activationfn
-        nextD = np.multiply(self.nn.weights[i][:, bias:].T * d[0], fn.grad(z[i]))
-        d.appendleft(nextD)
-
-      d.appendleft(None) # Filler so the indexes matchup
-
-      # Get gradients
-      grads = list()
-      for i in range(len(self.nn.weights)):
-        # Unregularized
-        tmpGrad = (1. / m) * d[i+1] * a[i].T
-        # Regularized
-        tmpGrad[:, bias:] += (self.learn_rate / m) * tmpGrad[:, bias:]
-        grads.append(tmpGrad)
-
-      # Unroll gradients
-      grad = np.hstack([g.flatten() for g in grads])
-
-      return cost, grad.T
-    return costFn
-  """
+    Returns:
+    A column vector representing the unrolled weight vector (copy).
+    """
+    weights = [np.matrix(weight) for weight in weights]
+    unrolled_list = [weight.ravel() for weight in weights]
+    return np.hstack(unrolled_list).T

test/nntrainer_test.py
@@ -4,7 +4,7 @@ from pymind.components import NeuralNetwork, NNTrainer
 from pymind.activationfn import *
 from pymind.errfn import *
 
-def generateMatrix(rows, columns, fn):
+def generateMatrix(rows, columns, fn = np.sin):
   """ Generates a matrix of a given shape using the passed function.
 
   Uses numpy.arange to create a row vector from 0 to the desired size, then reshapes the matrix to
@@ -20,12 +20,36 @@ def generateMatrix(rows, columns, fn):
   """
   size = rows * columns
   matrix = np.arange(size).reshape(rows, columns)
-  return fn(matrix)
+  return fn(np.matrix(matrix))
 
-def testCost1():
-  """ Tests cost function of the nntrainer class. """
+def computeNumericalGradient(fn, x, e = 1e-4):
+  """ Computes the gradient of a function using "finite differences".
 
-  # Test co
+  Arguments:
+  fn -- the function to compute the numerical gradient of
+  x -- the value to to evaluate the numerical gradient at
+  e -- the perturbation being used to focus the calculation (default: 1e-4)
+  Returns:
+  The gradient of fn at x
+
+  Note: x should be a column vector representing the input to the fn.
+  """
+  numGrad = np.matrix(np.zeros(x.shape))
+  perturb = np.matrix(np.zeros(x.shape))
+  for i in range(numGrad.size):
+    perturb[i] = e
+    pos = fn(x + e)
+    neg = fn(x - e)
+    numGrad[i] = (pos - neg) / (2. * e)
+    perturb[i] = 0
+
+  return numGrad
+
+def testCostAccuracy1():
+  """ Tests accuracy of cost function in the nntrainer class to 10 sig. digits.
+
+  Turns off regularization by setting the learning rate to 0.
+  """
   params = {
     "input_units": 10,
     "output_units": 6,
@@ -46,16 +70,54 @@ def testCost1():
   costfn = trainer.createCostfn(X, y, learn_rate, errfn)
 
   # Generate input weights
-  weights = list()
-  weights.append(generateMatrix(8, 11))
-  weights.append(generateMatrix(6, 9))
+  weights = [generateMatrix(weight.shape[0], weight.shape[1]) for weight in nnet.weights]
 
   # Run cost function
-  pass
+  cost, grad = costfn(weights)
+  actual = 5.40419468413414 # Calculated with Octave
 
-"""
-def testGradient():
-  # Create Neural NeuralNetwork
+  np.testing.assert_almost_equal(cost, actual, decimal=10,
+    err_msg = "Trainer cost function not accurate enough.")
+
+def testCostAccuracy2():
+  """ Tests accuracy of cost function in the nntrainer class to 10 sig. digits.
+
+  Turns on regularization by setting the learning rate to 1.
+  """
+  params = {
+    "input_units": 15,
+    "output_units": 5,
+    "hidden_units": 10,
+    "activationfn": [identity, sigmoid, sigmoid],
+    "bias": True
+  }
+  nnet = NeuralNetwork(params)
+  trainer = NNTrainer(nnet)
+
+  # Create cost function parameters
+  X = generateMatrix(15, 10)
+  y = generateMatrix(5, 10)
+  learn_rate = 1
+  errfn = logitError
+
+  # Create cost function (don't compute gradient)
+  costfn = trainer.createCostfn(X, y, learn_rate, errfn, False)
+
+  # Generate input weights
+  weights = [generateMatrix(weight.shape[0], weight.shape[1]) for weight in nnet.weights]
+
+  # Run cost function
+  cost = costfn(weights)
+  actual = 9.00036890107429 # Calculated with Octave
+
+  np.testing.assert_almost_equal(cost, actual, decimal=10,
+    err_msg = "Trainer cost function not accurate enough.")
+
+def testGradientAccuracy1():
+  """ Tests accuracy of the trainer's gradient calculation to 10 sig. digits.
+
+  Turns off regularization by setting learn_rate to 0.
+  """
   params = {
     "input_units": 10,
     "output_units": 6,
@@ -64,38 +126,49 @@ def testGradient():
     "bias": True
   }
   nnet = NeuralNetwork(params)
-  # Create NNTrainer
-  trainer = NNTrainer(nnet,logitError,1)
-  # Create Random Data
-  X = np.matrix(np.sin(-np.arange(-1,1,2.0/5000).reshape((10,500))**2))
-  y = np.matrix(np.sin(-np.arange(-1,1,2.0/3000).reshape((6,500))**2))
-  # Create Cost Function
-  costFn = trainer.createCostFn(X,y)
-  # Run Cost Function and retrieve gradient
-  wsize = sum([w.size for w in nnet.weights])
-  wvec = np.sin(3*np.arange(-1,1,2.0/wsize))
-  cst,grd = costFn(wvec)
-  # Run computeNumericalGradient with the network costs and weights
-  cgrd = computeNumericalGradient(costFn,wvec)
+  trainer = NNTrainer(nnet)
 
-  # for i in xrange(len(cgrd)):
-  #   print "%10.6f %10.6f" % (cgrd[i], grd[i])
-  for i in range(len(cgrd)):
-    np.testing.assert_array_almost_equal(cgrd[i], grd[i], decimal = 3,
-      err_msg = "The output grd at index %d should be \n %r \n != %r" % (i, cgrd[i], grd[i]))
+  # Create cost function parameters
+  X = generateMatrix(10, 3)
+  y = generateMatrix(6, 3)
+  learn_rate = 0
+  errfn = logitError
 
-def computeNumericalGradient(costFn, weights, e=0.000001):
-  wsize = weights.size
-  grd = np.empty(wsize)
-  for i in xrange(wsize):
-    w = weights[i]
-    w_inc = w + e
-    w_dec = w - e
-    weights[i] = w_inc
-    c_inc,_ = costFn(weights)
-    weights[i] = w_dec
-    c_dec,_ = costFn(weights)
-    grd[i] = (c_inc - c_dec)/(2*e)
-    weights[i] = w
-  return np.matrix(grd).reshape((wsize,1))
-"""
+  # Create cost function
+  costfn = trainer.createCostfn(X, y, learn_rate, errfn)
+
+  # Generate input weights
+  weights = [generateMatrix(weight.shape[0], weight.shape[1]) for weight in nnet.weights]
+
+  # Run cost function
+  cost, grad = costfn(weights)
+
+  # Simple unrolled cost function to use in computeNumericalGradient
+  costfn = trainer.createCostfn(X, y, learn_rate, errfn, False)
+  unrolledCostfn = lambda w: costfn(trainer.reshapeWeights(w))
+
+  testGrad = computeNumericalGradient(unrolledCostfn, trainer.unrollWeights(weights))
+  # print trainer.unrollWeights(grad)
+
+  np.testing.assert_array_almost_equal(trainer.unrollWeights(grad), testGrad, decimal = 10,
+      err_msg = "Trainer gradient calculation is not accurate enough.")
+
+def testUnrollReshapeWeights():
+  """ Tests if trainer weight unrolling and reshaping works properly. """
+  params = {
+    "input_units": 10,
+    "output_units": 6,
+    "hidden_units": 8,
+    "activationfn": [identity, sigmoid, sigmoid],
+    "bias": True
+  }
+  nnet = NeuralNetwork(params)
+  trainer = NNTrainer(nnet)
+
+  unrolledWeights = trainer.unrollWeights(nnet.weights)
+  reshapedWeights = trainer.reshapeWeights(unrolledWeights)
+
+  for i in range(len(reshapedWeights)):
+    np.testing.assert_array_equal(reshapedWeights[i], nnet.weights[i],
+      err_msg = "The reshapedWeight at index %d is not the same as the \
+      same weight in the neural network." % i)