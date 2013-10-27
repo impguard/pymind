99bf76670aa8c98aefd704aa0815ff96f1589efd
whsieh <whsieh@berkeley.edu>
2013-10-27 16:54:09 -0700

    changed fn_list to metric_list, combiner_list

--- pymind/metricfn.py
+++ pymind/metricfn.py
@@ -12,15 +12,15 @@ combines them, returning the resulting object.
 import numpy as np
 from util import assertType
 
-metric_fn_list = dict()
+metric_list = dict()
 def get_metric(name):
   assertType("metricfn.get_metric", "name", name, str)
-  assert name in fn_list, "(metricfn) %s cannot be found." % name
-  return fn_list[name]
+  assert name in metric_list, "(metricfn) %s cannot be found." % name
+  return metric_list[name]
 
 def add_metric(name, fn):
   assertType("metricfn.add_metric", "name", name, str)
-  fn_list[name] = fn
+  metric_list[name] = fn
 
 class _metricfn(object):
   @classmethod
@@ -31,15 +31,15 @@ class _metricfn(object):
   def _extract(cls, nnet):
     raise Exception("_extract not implemented")
 
-combiner_fn_list = dict()
+combiner_list = dict()
 def get_combiner(name):
   assertType("metricfn.get_combiner", "name", name, str)
-  assert name in fn_list, "(metricfn) %s cannot be found." % name
-  return fn_list[name]
+  assert name in metric_list, "(metricfn) %s cannot be found." % name
+  return combiner_list[name]
 
 def add_combiner(name, fn):
   assertType("metricfn.add_combiner", "name", name, str)
-  fn_list[name] = fn
+  combiner_list[name] = fn
 
 class _combinerfn(object):
   @classmethod
@@ -58,3 +58,5 @@ class ListCombiner(_combinerfn):
     else:
       # list.append would mutate total. Is this what we want? 
       return total + [res,]
+
+add_combiner("list_combiner",ListCombiner)
